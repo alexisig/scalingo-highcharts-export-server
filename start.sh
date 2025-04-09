10b47254bbb7e4996e6383852253271b833ec2b3
Alexis A. <alexis.athlani@gmail.com>
2025-04-09 10:38:46 +0200

    no sandbox

--- start.sh
+++ start.sh
@@ -1,4 +1,1 @@
-echo -e '#!/bin/bash\nexec google-chrome-stable --no-sandbox "$@"' > /usr/local/bin/chrome && \
-    chmod +x /usr/local/bin/chrome
-
-highcharts-export-server --enableServer 1 --port $PORT --enableDebug
+highcharts-export-server --enableServer 1 --port $PORT --enableDebug  --loadConfig config.json
